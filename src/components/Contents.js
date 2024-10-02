1ee65d5549fc9b2dd8407ec289307be7287bb618
alex song <dongyeop.song@gmail.com>
2024-10-02 13:56:36 +0900

    Add : Contents compoents

---
+++ src/components/Contents.js
@@ -0,0 +1,26 @@
+export default function Contents() {
+    return (
+        <div>
+            <ContentHeader />
+            <ContentBody />
+        </div>
+    )
+}
+
+function ContentHeader() {
+    return (
+        <div>
+            <h1>ContentHeader</h1>
+        </div>
+    )
+}
+
+function ContentBody() {
+    return (
+        <div>
+            <h1>ContentBody</h1>
+        </div>
+    )
+}
+
+
